576c9eebdb870f57018af2b61ae03e4f0d0f5669
Solomon Bestz <kingsolodawarrior@gmail.com>
2022-06-22 13:46:32 +0100

    styled the button

--- youtube_download.py
+++ youtube_download.py
@@ -12,8 +12,7 @@ import shutil
 # Default Variable Instantiation
 screen = Tk()
 title = screen.title('Youtube Downloader By Bestz Group')
-screen.minsize(700, 600)
-screen.maxsize(700, 600)
+screen.resizable(False, False)
 screen.iconbitmap('media/favicon.ico')
 canvas = Canvas(screen, width= 700, height=600)
 canvas.pack()
@@ -46,30 +45,30 @@ logo_img = PhotoImage(file='media/yt.png')
 logo_img = logo_img.subsample(2, 2)
 
 #link inpot field
-link_field = Entry(screen, width=50)
+link_field = Entry(screen, borderwidth=0, highlightthickness=0)
 link_field.insert(END, "Enter link here")
 link_label = Label(screen, text='Bestz Youtube Video Link', font=('Arial', 15))
 
 #Select Path for saving the file
 path_label = Label(screen, text="Select Path For Download: ", font=('Arial', 10))
-path_field = Entry(screen, width=50)
+path_field = Entry(screen, borderwidth=0, highlightthickness=0)
 path_field.insert(END, "Path to save downloaded video here.")
-select_btn = Button(screen, text='Select', command=select_path)
+select_btn = Button(screen, text='Select', command=select_path, border=0, background='red', height=2)
 
 #Download buttons
-download_btn = Button(screen, text="Download", command=download_file)
+download_btn = Button(screen, text="Download", command=download_file, border=0, background= 'red', height=2)
 
 #Add logo image to the windows 
 canvas.create_image(350, 60, image=logo_img)
 
 #Add the widgets to windows
 canvas.create_window(350, 170, window=link_label)
-canvas.create_window(350, 220, window=link_field)
+canvas.create_window(350, 220, width=250, height=30, window=link_field)
 
 #Add the widgets to window path to download 
 canvas.create_window(160, 280, window=path_label)
-canvas.create_window(390, 280, window=path_field)
-canvas.create_window(570, 280, window=select_btn)
+canvas.create_window(370, 280, width=250, height=30, window=path_field)
+canvas.create_window(520, 280, window=select_btn)
 
 #add the widget to window download button 
 canvas.create_window(350, 350, window=download_btn)
